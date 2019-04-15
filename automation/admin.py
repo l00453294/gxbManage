0fda4b0f19395867b2f7db181730f13126bac776
l00453294 <liukang29@huawei.com>
2019-04-15 23:59:30 +0800

    Initial commit

---
+++ automation/admin.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from .models import TestPartner, DeviceStandard, DeviceTemplate
+# Register your models here.
+
+
+@admin.register(DeviceStandard)
+class DeviceStandardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'DevName', 'DevType', 'SimIntro', 'MAC', 'IPV4', 'IPV6', 'ThroughPut', 'PacketForwarding',
+                    'Queue', 'CreateTime', 'LastEditTime')
+
+
+@admin.register(TestPartner)
+class DeviceSpecAdmin(admin.ModelAdmin):
+    list_display = ('id', 'TestOrganization', 'TestUserName', 'Tel', 'Email', 'SpecManager', 'CreateTime',
+                    'LastEditTime')
+    list_per_page = 2                                                   # 每页显示5条数据
+    ordering = ['id']                                                   # 按照id排列，默认是升序
+    list_filter = ('TestOrganization', 'TestUserName')                  # 过滤器
+    search_fields = ('Tel', 'Email', 'TestUserName', 'TestOrganization')# 查询器
+    date_hierarchy = 'CreateTime'
+
+
+@admin.register(DeviceTemplate)
+class DeviceTemplateAdmin(admin.ModelAdmin):
+    list_display = ('TemVersion', 'TemType', 'TemPath', 'TemManager', 'CreateTime', 'LastEditTime')
+
+
+admin.site.site_header = '工信部入网管理系统'
+admin.site.site_title = '工信部入网'
+
+
---
+++ automation/models.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+
+
+class DeviceStandard(models.Model):                                                           # 设备参数表
+    class Meta:                                                                               # 起别名：规格参数表
+        verbose_name_plural = u'设备规格参数'
+        verbose_name = u'《设备参数表》'
+
+    DevName = models.CharField(u'设备名称', max_length=20)
+    DevType = models.CharField(u'默认以太交换机', max_length=20, default=u'以太交换机')
+    SimIntro = models.TextField(u"设备的接口和电源简单介绍", null=False)
+    MAC = models.IntegerField()
+    IPV4 = models.IntegerField()
+    IPV6 = models.IntegerField()
+    ThroughPut = models.IntegerField()
+    PacketForwarding = models.IntegerField()
+    Queue = models.IntegerField()
+    IsDelete = models.BooleanField(default=False)           # 默认不删除
+    CreateTime = models.DateTimeField(u'创建时间', default=timezone.now)
+    LastEditTime = models.DateTimeField(u'最后修改时间', auto_now=True)
+
+    def __unicode__(self):
+        return "deviceName:"+self.DevName
+
+
+class DeviceTemplate(models.Model):                                                            # 入网模板表
+    class Meta:                                                                                # 起别名：入网模板表
+        verbose_name_plural = '入网设备模板'
+        verbose_name = '《入网模板》'
+    TemVersion = models.IntegerField(verbose_name=u'版本', default=2)                          # 模板版本，默认选择第二种
+    TemType = models.CharField(verbose_name=u'类型', max_length=20, default=u'以太交换机')     # 默认是以太交换机
+    TemPath = models.CharField(verbose_name=u'模板路径', max_length=20, null=True)             # 允许为null
+    TemManager = models.CharField(verbose_name=u'管理员', max_length=20, default=" ")          # 管理员创建规格信息模板
+    IsDelete = models.BooleanField(default=False)                                              # 默认不删除
+    CreateTime = models.DateTimeField(u'创建时间', default=timezone.now)
+    LastEditTime = models.DateTimeField(u'最后修改时间', auto_now=True)
+
+    def __unicode__(self):
+        return "deviceName:"+self.TemManager
+
+
+class TestPartner(models.Model):
+    class Meta:                                                                               # 起别名：测试机构人员表
+        verbose_name_plural = '测试机构客户'
+        verbose_name = u'《机构客户》'
+    TestOrganization = models.CharField(verbose_name=u'测试机构', max_length=20)              # 测试机构
+    TestUserName = models.CharField(verbose_name=u'测试人员', max_length=20)                  # 测试人员
+    Tel = models.BigIntegerField(verbose_name=u'联系方式')                                    # 联系方式
+    Email = models.CharField(verbose_name=u'邮箱', max_length=30, default=" ")                # email
+    SpecManager = models.CharField(verbose_name=u'管理员', max_length=20, default="")         # 管理员创建规格信息模板
+    IsDelete = models.BooleanField(default=False)                   # 默认不删除
+    CreateTime = models.DateTimeField(u'创建时间', default=timezone.now)
+    LastEditTime = models.DateTimeField(u'最后修改时间', auto_now=True)
+
+    def __unicode__(self):
+        return "TestPartner:"+str(self.Tel)
+
+
+class DeviceSpec(models.Model):
+    class Meta:                                                                               # 起别名：测试机构人员表
+        verbose_name_plural = '测试机构客户'
+        verbose_name = u'《机构客户》'
+    TestOrganization = models.CharField(verbose_name=u'测试机构', max_length=20)              # 测试机构
+    TestUserName = models.CharField(verbose_name=u'测试人员', max_length=20)                  # 测试人员
+    Tel = models.BigIntegerField(verbose_name=u'联系方式')                                    # 联系方式
+    Email = models.CharField(verbose_name=u'邮箱', max_length=30, default=" ")                # email
+    SpecManager = models.CharField(verbose_name=u'管理员', max_length=20, default="")         # 管理员创建规格信息模板
+    IsDelete = models.BooleanField(default=False)                   # 默认不删除
+    CreateTime = models.DateTimeField(u'创建时间', default=timezone.now)
+    LastEditTime = models.DateTimeField(u'最后修改时间', auto_now=True)
+
+    def __unicode__(self):
+        return "TestPartner:"+str(self.Tel)
